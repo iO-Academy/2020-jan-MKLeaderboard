7ed0bf77b16609999344e3e10948845b5fe2c9c4
Alex Stone Lunde <alexlunde@gmail.com>
2020-04-21 16:26:20 +0100

    added cors to node server

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./Config/routes');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
