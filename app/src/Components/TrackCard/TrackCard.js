f09fc1061c1640ed9129291e9a8064344d01cc17
Alex Stone Lunde <alexlunde@gmail.com>
2020-04-21 15:42:07 +0100

    initial test build of TrackContainer

---
+++ app/src/Components/TrackCard/TrackCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+TrackCard = () => {
+    return <h1>{ this.props.trackCard.name }</h1>
+}
+
+export default TrackCard
---
+++ app/src/Components/TrackContainer/TrackContainer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import TrackCard from '../../Components/TrackCard/TrackCard';
+
+export default class TracksContainer extends Component {
+
+    state = {
+        trackCards: []
+    };
+
+    componentDidMount() {
+
+        fetch('localhost:4000/tracks')
+            .then(res => res.json())
+            .then((responseData) => {
+
+                this.setState({ trackCards: responseData.data });
+
+                let trackCardComponents = this.trackCards.map((trackCard) => {
+                    <TrackCard key={trackCard.id} trackCardContent={trackCard} />
+                });
+
+                render() {
+                    return (
+                        <div>
+                        { trackCardComponents }
+                        </div>
+                    )
+                }
+
+            })
+            .catch(console.log);
+    }
+}
