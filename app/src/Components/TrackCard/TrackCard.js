e20582e6080fe1ebeb1f449aa787fdae8c91f030
mathewjmartin <matthewjmartin@zoho.com>
2020-04-22 15:37:46 +0100

    finalised css styling for trackCard component

--- app/src/Components/TrackCard/TrackCard.js
+++ app/src/Components/TrackCard/TrackCard.js
@@ -5,12 +5,8 @@ class TrackCard extends Component {
     render() {
         return (
             <div className="trackCard">
-            <img className="trackCardImg" src={ this.props.trackCardContent.favChar.url } alt={ this.props.trackCardContent.favChar.name } />
-        <h3>{ this.props.trackCardContent.name }</h3>
-        <p className="dataLabel">Cohort:</p>
-        <p>{ this.props.racerCardContent.cohort.name }</p>
-        <p className="dataLabel">Favourite Character:</p>
-        <p>{ this.props.racerCardContent.favChar.name }</p>
+            <img className="trackCardImg" src={ this.props.trackCardContent.img } alt={ this.props.trackCardContent.img } />
+            <h3>{ this.props.trackCardContent.name }</h3>
         </div>
     );
     }
