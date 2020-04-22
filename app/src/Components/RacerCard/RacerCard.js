74cc6a439d8e0002258d56c3e4855b5f35b49c78
mathewjmartin <matthewjmartin@zoho.com>
2020-04-22 14:24:47 +0100

    created new trackCard files

--- app/src/Components/RacerCard/RacerCard.js
+++ app/src/Components/RacerCard/RacerCard.js
@@ -1,33 +1,16 @@
 import React, {Component} from 'react';
 import './racerCard.css'
 
-
 class RacerCard extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {racers: []}
-    // }
-    // componentDidMount() {
-    //     fetch( 'http://localhost:4000/users')
-    //         .then( data => data.json() )
-    //         .then( data => console.log(data))
-    //         // .then( ({data: racers}) => this.setState({racers}))
-    //     // console.log(this.state)
-    // }
-
-                // <img className="racerCardImg" src={ racerCardContent.favChar.url } alt={ racerCardContent.favChar.name } />
-                // <h3>{ racerCardContent.name }</h3>
-                // <p>Cohort: { racerCardContent.cohort.name }</p>
-                // <p>Favourite Character: { racerCardContent.favChar.name }</p>
     render() {
         return (
             <div className="racerCard">
-                <img className="racerCardImg" src="./images/mario.png"/>
-                <h3>Michael123</h3>
+                <img className="racerCardImg" src={ this.props.racerCardContent.favChar.url } alt={ this.props.racerCardContent.favChar.name } />
+                <h3>{ this.props.racerCardContent.name }</h3>
                 <p className="dataLabel">Cohort:</p>
-                <p>Naked Molerats</p>
+                <p>{ this.props.racerCardContent.cohort.name }</p>
                 <p className="dataLabel">Favourite Character:</p>
-                <p>Baby Bowser</p>
+                <p>{ this.props.racerCardContent.favChar.name }</p>
             </div>
         );
     }
--- app/src/Components/TrackCard/TrackCard.js
+++ app/src/Components/TrackCard/TrackCard.js
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, {Component} from 'react';
+import './trackCard.css'
 
-const TrackCard = ({ trackCardContent }) => {
-    return <h1>{ trackCardContent.name }</h1>
+class TrackCard extends Component {
+    render() {
+        return (
+            <div className="trackCard">
+            <img className="trackCardImg" src={ this.props.trackCardContent.favChar.url } alt={ this.props.trackCardContent.favChar.name } />
+        <h3>{ this.props.trackCardContent.name }</h3>
+        <p className="dataLabel">Cohort:</p>
+        <p>{ this.props.racerCardContent.cohort.name }</p>
+        <p className="dataLabel">Favourite Character:</p>
+        <p>{ this.props.racerCardContent.favChar.name }</p>
+        </div>
+    );
+    }
 }
+
+export default TrackCard
